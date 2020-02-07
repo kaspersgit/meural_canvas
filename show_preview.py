35841e83d27f4c10aab1cbf8d8e6e730a3e9738c
kasper.de-harder <kasper.de-harder@klarna.com>
2020-02-07 23:55:09 +0100

    Only authenticae when no token or 300 second old token and load in favorites from csv

--- show_preview.py
+++ show_preview.py
@@ -1,5 +1,6 @@
 # Import modules
-import pandas as pd 
+import pandas as pd
+import time
 import stringdist
 import random
 import requests
@@ -7,15 +8,7 @@ import sys
 import json
 import pandas as pd
 
-# set variables
-path_to_json = "/home/pi/Documents/trusted/meural_cred.json"
-
-with open(path_to_json, "r") as handler:
-    info = json.load(handler)
-
-username = info["username"]
-password = info["password"]
-
+# Meural api base url
 base_url = "https://api.meural.com/v0"
 
 # Meural api functions
@@ -24,19 +17,8 @@ def authenticate(path="/authenticate"):
         base_url + path, data={"username": username, "password": password}
     ).json()
     token = req["token"]
-    return token
-
-
-def get_uploads(token, path="/user/items?count=50&page=1"):
-    headers = {"Authorization": "Token " + token}
-    items = requests.get(base_url + path, headers=headers).json()
-    return items
-
-
-def get_favorites(token, path="/favorites/items?count=50&page=1"):
-    headers = {"Authorization": "Token " + token}
-    items = requests.get(base_url + path, headers=headers).json()
-    return items
+    token_time = time.time()
+    return token, token_time
 
 
 def preview_item(token, art_id, path="/devices/8292/preview/"):
@@ -44,33 +26,34 @@ def preview_item(token, art_id, path="/devices/8292/preview/"):
     preview = requests.post(base_url + path + str(art_id), headers=headers).json()
 
 
-authors = []
-names = []
-descriptions = []
-art_ids = []
-token = authenticate()
-items = get_favorites(token)
+# Check if token time exist otherwise authenticate
+if "token_time" not in globals():
+    token, token_time = authenticate()
+    # set variables
+    path_to_json = "/home/pi/Documents/trusted/meural_cred.json"
 
-# number of items uploaded in items['count']
-for i in range(items["count"]):
-    authors.append(items["data"][i]["author"])
-    names.append(items["data"][i]["name"])
-    descriptions.append(items["data"][i]["description"])
-    art_ids.append(items["data"][i]["id"])
+    with open(path_to_json, "r") as handler:
+        info = json.load(handler)
 
-uploads = pd.DataFrame(
-    {"author": authors, "name": names, "description": descriptions, "art_id": art_ids}
+    username = info["username"]
+    password = info["password"]
+# If token is older than 300 seconds than re-authenticate
+elif (token_time - time.time()) > 300:
+    token, token_time = authenticate()
+
+favorites = pd.read_csv(
+    "/home/pi/Documents/python_scripts/meural_canvas/meural_favorites.csv"
 )
 
 # Preview smalles levenshtein distance author
 def preview_author(author="rembrandt"):
     distance = {}
-    for n in filter(None, uploads.author):
+    for n in filter(None, favorites.author):
         lh = stringdist.levenshtein_norm(author, n)
         distance[n] = lh
 
     chosen_author = min(distance, key=distance.get)
-    art_ids = uploads[uploads['author']==chosen_author]['art_id']
+    art_ids = uploads[uploads["author"] == chosen_author]["art_id"]
     chosen_art_id = random.choice(list(art_ids))
     print(chosen_art_id)
     # preview item found by keyword on device 8292
