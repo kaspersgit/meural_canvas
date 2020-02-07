6d493256fab744e429442dd7e4a58172cb734f6d
kasper.de-harder <kasper.de-harder@klarna.com>
2020-02-08 00:05:43 +0100

    Added run time in seconds at different steps

--- show_preview.py
+++ show_preview.py
@@ -1,5 +1,4 @@
 # Import modules
-import pandas as pd
 import time
 import stringdist
 import random
@@ -26,6 +25,24 @@ def preview_item(token, art_id, path="/devices/8292/preview/"):
     preview = requests.post(base_url + path + str(art_id), headers=headers).json()
 
 
+# Preview smalles levenshtein distance author
+def preview_author(author="rembrandt"):
+    distance = {}
+    for n in filter(None, favorites.author):
+        lh = stringdist.levenshtein_norm(author, n)
+        distance[n] = lh
+
+    chosen_author = min(distance, key=distance.get)
+    art_ids = favorites[favorites["author"] == chosen_author]["art_id"]
+    chosen_art_id = random.choice(list(art_ids))
+    print(chosen_art_id)
+    # preview item found by keyword on device 8292
+    preview_item(token, chosen_art_id)
+
+
+# time it
+print("---functions loaded %s seconds ---" % (time.time() - start_time))
+
 # Check if token time exist otherwise authenticate
 if "token_time" not in globals():
     # set variables
@@ -43,23 +60,14 @@ if "token_time" not in globals():
 elif (token_time - time.time()) > 300:
     token, token_time = authenticate()
 
+# time it
+print("---authenticated %s seconds ---" % (time.time() - start_time))
+
 favorites = pd.read_csv(
     "/home/pi/Documents/python_scripts/meural_canvas/meural_favorites.csv"
 )
 
-# Preview smalles levenshtein distance author
-def preview_author(author="rembrandt"):
-    distance = {}
-    for n in filter(None, favorites.author):
-        lh = stringdist.levenshtein_norm(author, n)
-        distance[n] = lh
-
-    chosen_author = min(distance, key=distance.get)
-    art_ids = favorites[favorites["author"] == chosen_author]["art_id"]
-    chosen_art_id = random.choice(list(art_ids))
-    print(chosen_art_id)
-    # preview item found by keyword on device 8292
-    preview_item(token, chosen_art_id)
-
-
 preview_author(sys.argv[1])
+
+# time it
+print("---finished %s seconds ---" % (time.time() - start_time))
