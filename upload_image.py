819112399fdc21cf60006c299ab779e45b1ab827
kasper.de-harder <kasper.de-harder@klarna.com>
2020-06-06 02:13:14 +0200

    Resizing cropping and uploading added

--- upload_image.py
+++ upload_image.py
@@ -8,6 +8,9 @@ import time
 
 import random
 import requests
+import warnings
+import urllib.request
+from PIL import Image
 
 # Uncomment for when you want to time the script
 #print("---requests loaded %s seconds ---" % round((time.time() - start_time),2))
@@ -30,27 +33,50 @@ def authenticate(path="/authenticate"):
     token_time = time.time()
     return token, token_time
 
+def resize_image(original_image):
+    meural_res = [1920,1080]
+    aspect_ratio_meural = meural_res[0]/meural_res[1]
+
+    image = Image.open('image_now.jpg')
+    # resolution of image
+    w, h = image.size
+    if (w < meural_res[0]) | (h < meural_res[1]):
+        print(1)
+        warnings.warn("Original image has lower resolution than Meural Canvas, may lead to unsharp image")
+    # get the w/h ratio (to preserve after scaling down)
+    aspect_ratio = w/h
+
+    # How much both width and height have to be resized (ratio)
+    resize_ratios = [meural_res[0]/w, meural_res[1]/h]
+    resize_size = [None] * 2
+    for i,j in enumerate(resize_ratios):
+        print(f'Does dimension {i} need to be resized? ' + str(j == max(resize_ratios)))
+        if j == max(resize_ratios):
+            resize_size[i]=int(1 * meural_res[i])
+        else:
+            resize_size[i]=int(max(resize_ratios) * image.size[i])
+    try:
+        resized_image = image.resize((resize_size[0], resize_size[1]))
+    except ValueError:
+        print("Invalid input to resizing")
+
+    # Cropping the image to make it fit perfectly
+    left_top = (int(resize_size[0]/2 - meural_res[0]/2), int(resize_size[1]/2 - meural_res[1]/2))
+    right_bottom = (left_top[0] + meural_res[0], left_top[1] + meural_res[1])
+    image_area = (left_top + right_bottom)
+    final_image = resized_image.crop(image_area)
+    return(final_image)
 
 def preview_item(token, art_id, path="/devices/8292/preview/"):
     headers = {"Authorization": "Token " + token}
     preview = requests.post(base_url + path + str(art_id), headers=headers).json()
 
 
-# Preview smalles levenshtein distance author
-def preview_author(author="rembrandt"):
-    distance = {}
-    for n in filter(None, favorites.keys()):
-        lh = stringdist.levenshtein_norm(author, n)
-        distance[n] = lh
-
-    chosen_author = min(distance, key=distance.get)
-    art_ids = favorites[chosen_author]
-    chosen_art_id = random.choice(art_ids)
-    #print(chosen_art_id)
-    # preview item found by keyword on device 8292
-    preview_item(token, chosen_art_id)
-    return(author_artwork[chosen_art_id],chosen_author)
-
+def upload_image(token, image_path, path="/items"):
+    headers = {"Authorization": "Token " + token}
+    files = {'image': open(image_path, 'rb')}
+    r = requests.post(base_url + path, headers=headers, files=files).json()
+    return(r)
 
 # time it
 # Uncomment for when you want to time the script
@@ -77,8 +103,6 @@ elif (token_time - time.time()) > 300:
 # Uncomment for when you want to time the script
 #print("---authenticated %s seconds ---" % round((time.time() - start_time),2))
 
-# Importing required libraries
-import urllib.request
 
 # Adding information about user agent
 # chance otherwise is that website block when they see python trying to access
@@ -86,36 +110,19 @@ opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
+#%%
 # setting filename and image URL
 filename = 'image_now.jpg'
-image_url = 'https://ichef.bbci.co.uk/onesport/cps/800/cpsprodpb/1384/production/_111769940_whatsubject.jpg'
+image_url = sys.argv[1]
 
 # calling urlretrieve function to get resource
 urllib.request.urlretrieve(image_url, filename)
 
-from PIL import Image
-
-meural_res = [1920,1080]
-image = Image.open('image_now.jpg')
-w, h = image.size
-aspect_ratio = w/h
-resize_ratios = [meural_res[0]/w, meural_res[1]/h]
-for i,j in enumerate(resize_ratios):
-    print(j == max(resize_ratios))
-    if j == max(resize_ratios):
-        resize_ratio[i]=1 * meural_res[i]
-    else:
-        resize_ratio[i]=aspect_ratio * meural_res[i]
-try:
-    resized_image = image.resize((resize_ratio[0], resize_ratio[1]))
-except ValueError:
-    print("Invalid input to resizing")
+final_image.save('resized_image.jpg')
+response = upload_image(token,"/Users/kasper.de-harder/Downloads/resized_image.jpg")
+preview_item(token, art_id = response['data']['id'])
 
 ###########################################################
-
-artwork_name, author = preview_author(sys.argv[1])
-print("Showing {} made by {}".format(artwork_name, author))
-
 
 # time it
 # Uncomment for when you want to time the script
